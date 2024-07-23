bced8f31087dfee20c208dc2e307b8a57b1fbc2c
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-07-23 11:59:20 +0100

    initial commit

---
+++ src/test/java/ma/internship/greenway/GreenwayApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.internship.greenway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
